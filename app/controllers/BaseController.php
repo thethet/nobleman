1775b64b9171fd637fdce51589333b090439ea54
thethet <thetthetaye2709@gmail.com>
2016-11-10 17:22:40 +0800

    Update BaseController.php

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -12,6 +12,7 @@ class BaseController extends Controller {
 		if ( ! is_null($this->layout))
 		{
 			$this->layout = View::make($this->layout);
+			//$this->layout = View::make($this->layout);
 		}
 	}
 
